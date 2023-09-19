30659592a1cd8f487450f59ac1b5cf597f060714
Rahmouni <imad19rahmouni@gmail.com>
2023-09-19 16:43:14 +0200

    last commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style-1.css">
+    <title>magic-store</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo"><i class="fa-brands fa-studiovinari"></i> Magic-<span>store</span></a>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="about">
+
+        <div class="row">
+
+            <div class="box">
+                <img src="images/about-img-1.png" alt="">
+                <h3>why choose us?</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam deserunt iusto aperiam veniam sed ipsum debitis ex expedita adipisci, recusandae nihil repellat quos voluptates perferendis excepturi a inventore vel.</p>
+            </div>
+
+            <div class="box">
+                <img src="images/about-img-2.png" alt="">
+                <h3>what we provide?</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam deserunt iusto aperiam veniam sed ipsum debitis ex expedita adipisci, recusandae nihil repellat quos voluptates perferendis excepturi a inventore vel.</p>
+            </div>
+
+        </div>
+
+    </section>
+
+</body>
+</html>
